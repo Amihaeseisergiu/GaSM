b29fbba5f7077bbc115ffc105ddb5d88512a4cc4
Cezar375 <sspeedy731@gmail.com>
2020-04-10 16:20:04 +0300

    some fixes

--- app/init.php
+++ app/init.php
@@ -2,7 +2,9 @@
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
+session_start();
 
-
+if(!isset($_SESSION['loggedIn'])) //daca nu e setata deja, ca sa nu se schimbe cand dau refresh la pagina 
+ $_SESSION['loggedIn']="false"; //initializam cu false sesiunea pt loggedIn
 
 ?>
