78a0d02d4c3e2bc547dbc02ef0c7fafdebfecdfe
AlexV-coder <61702518+AlexV-coder@users.noreply.github.com>
2020-04-17 11:09:33 +0300

    Update Statistics.php

--- app/controllers/Statistics.php
+++ app/controllers/Statistics.php
@@ -71,15 +71,7 @@ class Statistics extends Controller
         }
 
 
-        if ($filter == "Weekly") {
-            $Pmarkers = $marker->getPrecedentTrash("weekly");
-        } else if ($filter == "Monthly") {
-            $Pmarkers = $marker->getPrecedentTrash("monthly");
-        } else if ($filter == "Daily") {
-            $Pmarkers = $marker->getPrecedentTrash("daily");
-        } else {
-            $Pmarkers = $marker->getTrash();
-        }
+        $Pmarkers = $marker->getPrecedentTrash($filter);
         $Pplastics = array();
         $Ppapers = array();
         $Pmetals = array();
@@ -177,8 +169,11 @@ class Statistics extends Controller
             $dif = '- ' . $dif . ' Reports';
             $arrow = 'uparrow';
         }
-        $markersByCounty = $marker->getMarkersByCounty();
-        asort($markersByCounty);
+        $markersByCounty = $marker->getMarkersByCounty($filter);
+        usort($markersByCounty, function($a, $b) {
+            return $a['quantity'] - $b['quantity'];
+        });
+        $marker->getMarkersByRegion();
         array_push($changes, array("arrow" => $arrow, "diff" => $dif));
         $this->view('statistics', ['plastics' => $plastics, 'papers' => $papers, 'glasses' => $glasses, 'metals' => $metals, 'garbageToShow' => $shownGarbage, 'timeFilter' => $filter, 'changes' => $changes, 'markersByCounty' => $markersByCounty]);
     }
