b02635fc7b205e0ea40d4412bd526e603e3030d6
AlexV-coder <alexvasile99@yahoo.com>
2020-05-08 15:13:47 +0300

    changed css

--- app/php/ajaxHTML.php
+++ app/php/ajaxHTML.php
@@ -66,11 +66,14 @@ if (isset($data['timeFilter'])) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Report</title>
+        <style type="text/css"></style>
     </head>
     <body>
         
     </body>
     </html>');
+    $style = file_get_contents('../css/htmlCss.css');
+    $doc->getElementsByTagName("style")->item(0)->nodeValue = $style;
     $str = file_get_contents(__DIR__ . '../initChart.php');
     $str = str_replace('<?php echo $pls; ?>', $data['plastic'], $str);
     $str = str_replace('<?php echo $pap; ?>', $data['paper'], $str);
@@ -86,16 +89,8 @@ if (isset($data['timeFilter'])) {
         $unit = "day";
     }
     $str = str_replace('<?php echo $valUnit; ?>', $unit, $str);
-  /*  $str = str_replace('<?php if ($data[\'garbageToShow\'][\'plastic\'] === true) echo \'true\';
-                            else echo \'false\'; ?>', 'true', $str);
-    $str = str_replace('<?php if ($data[\'garbageToShow\'][\'paper\'] === true) echo \'true\';
-                            else echo \'false\'; ?>', 'true', $str);
-    $str = str_replace('<?php if ($data[\'garbageToShow\'][\'glass\'] === true) echo \'true\';
-                            else echo \'false\'; ?>', 'true', $str);
-    $str = str_replace('<?php if ($data[\'garbageToShow\'][\'metal\'] === true) echo \'true\';
-                            else echo \'false\'; ?>', 'true', $str);*/
     $header = $doc->createElement('h1');
-    $header->setAttribute('style', 'display:flex; justify-content:center; align-items:center; background-color: #cacbc8; margin-top:0em; font-size:3em; padding-bottom:1em; padding-top: 0.5em;');
+    $header->setAttribute('id', 'header');
     if ($data['timeFilter'] === "AllTime" || $data['timeFilter'] == '') {
         $data['timeFilter'] = "All Time";
     }
@@ -112,7 +107,7 @@ if (isset($data['timeFilter'])) {
     }
     $header->appendChild($nodText2);
     $divNode = $doc->createElement('div');
-    $divNode->setAttribute('style', 'width: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;');
+    $divNode->setAttribute('id', 'divNode');
     $bodyContent = $doc->getElementsByTagName('body');
     $firstScript = $doc->createElement('script');
     $firstScript->setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js');
@@ -129,9 +124,9 @@ if (isset($data['timeFilter'])) {
         foreach ($markersByCounty as $county) {
             $listEl = $doc->createElement('div');
             if ($contor <= 5) {
-                $listEl->setAttribute('style', 'background-color: #0ed145;');
+                $listEl->setAttribute('class', 'listElGreen');
             } else if ($contor > count($markersByCounty) - 5) {
-                $listEl->setAttribute('style', 'background-color: #e03131;');
+                $listEl->setAttribute('class', 'listElRed');
             }
             $contor++;
             $txt = $doc->createTextNode($county['county'] . ' : ' . $county['quantity']);
@@ -140,46 +135,46 @@ if (isset($data['timeFilter'])) {
         }
     }
     foreach ($bodyContent as $body) {
-        $body->setAttribute('style', 'background-color: #e7e7e7;');
+        $body->setAttribute('id', 'body');
         $body->appendChild($firstScript);
         $body->appendChild($header);
         $body->appendChild($divNode);
         $body->appendChild($secondScript);
     }
     $childDivNode1 = $doc->createElement('div');
-    $childDivNode1->setAttribute('style','border: 5px solid black; margin-bottom: 3em; height : 500px; width: 90%;');
+    $childDivNode1->setAttribute('id','childDivNode1');
     $childCanvasNode1 = $doc->createElement('canvas');
-    $childCanvasNode1->setAttribute('style', 'width:100%; height:100%;');
+    $childCanvasNode1->setAttribute('class', 'childCanvasNode1');
     $childCanvasNode1->setAttribute('id', 'lineChart');
     $childDivNode1->appendChild($childCanvasNode1);
     $divNode->appendChild($childDivNode1);
     $childDivNode2 = $doc->createElement('div');
-    $childDivNode2->setAttribute('style','border: 5px solid black; margin-bottom: 3em; height : 500px; width: 90%;');
+    $childDivNode2->setAttribute('id','childDivNode2');
     $childCanvasNode2 = $doc->createElement('canvas');
-    $childCanvasNode2->setAttribute('style', 'height: 100%; width : 100%;');
+    $childCanvasNode2->setAttribute('class', 'childCanvasNode2');
     $childCanvasNode2->setAttribute('id', 'barChart');
     $childDivNode2->appendChild($childCanvasNode2);
     $divNode->appendChild($childDivNode2);
     $childDivNode3 = $doc->createElement('div');
-    $childDivNode3->setAttribute('style','border: 5px solid black; margin-bottom: 3em; height : 300px; width : 300px;');
+    $childDivNode3->setAttribute('id','childDivNode3');
     $childCanvasNode3 = $doc->createElement('canvas');
+    $childCanvasNode3->setAttribute('class', 'childCanvasNode3');
     $childCanvasNode3->setAttribute('id', 'pieChart');
-    $childCanvasNode3->setAttribute('style', 'width : 100%; height : 100%;');
     $childDivNode3->appendChild($childCanvasNode3);
     $divNode->appendChild($childDivNode3);
    // $divNode->appendChild($childDivNode2);
-    $list->setAttribute('style', 'display: flex; justify-content:center; flex-direction:column; font: bold;');
+    $list->setAttribute('class', 'list');
     $divCountry = $doc->createElement('div');
-    $divCountry->setAttribute('style', 'width: 25%; text-align: center; border: 2px solid black; background-color: white; margin-top:2em; font-family: sens-serif;');
-    $hdr->setAttribute('style', 'background-color: #cacbc8; padding-top: 1em; padding-bottom: 1em; margin-bottom: 0em; margin-top: 0em; font-size: 1.5em;');
+    $divCountry->setAttribute('class', 'divCountryAndRegion');
+    $hdr->setAttribute('class', 'headerList');
     $divCountry->appendChild($hdr);
     $divCountry->appendChild($list);
     $hdr2 = $doc->createElement('h2');
-    $hdr2->setAttribute('style', 'background-color: #cacbc8; padding-top: 1em; padding-bottom: 1em; margin-bottom: 0em; margin-top: 0em; font-size: 1.5em;');
+    $hdr2->setAttribute('class', 'headerList');
     $txt = $doc->createTextNode('County Level');
     $hdr2->appendChild($txt);
     $divRegion = $doc->createElement('div');
-    $divRegion->setAttribute('style', 'width: 25%; text-align: center; border: 2px solid black; background-color: white; margin-top:2em; font-family: sens-serif;');
+    $divRegion->setAttribute('class', 'divCountryAndRegion');
     $list2 = $doc->createElement('div');
     $divRegion->appendChild($hdr2);
     $divRegion->appendChild($list2);
@@ -189,9 +184,9 @@ if (isset($data['timeFilter'])) {
         foreach ($markersByRegion as $region) {
             $listEl = $doc->createElement('div');
             if ($contor <= 5) {
-                $listEl->setAttribute('style', 'background-color: #0ed145;');
+                $listEl->setAttribute('class', 'listElGreen');
             } else if ($contor > count($markersByRegion) - 5) {
-                $listEl->setAttribute('style', 'background-color: #e03131;');
+                $listEl->setAttribute('class', 'listElRed');
             }
             $contor++;
             $txt = $doc->createTextNode($region['city'] . ' : ' . $region['quantity']);
@@ -200,7 +195,7 @@ if (isset($data['timeFilter'])) {
         }
     }
     $divCountryAndRegion = $doc->createElement('div');
-    $divCountryAndRegion->setAttribute('style', 'display: flex; justify-content: space-around;');
+    $divCountryAndRegion->setAttribute('id', 'divCountryAndRegionContainer');
     $divCountryAndRegion->appendChild($divCountry);
     $divCountryAndRegion->appendChild($divRegion);
     $body->appendChild($divCountryAndRegion);
